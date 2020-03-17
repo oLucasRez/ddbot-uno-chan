1a3c1771442e343627cc235cd8a2996c06fa08bd
Riccardo Cafagna <DesktopRicc+RiccardoCafa@users.noreply.github.com>
2020-03-17 01:08:10 -0300

    Reaction Images using google, still working on it

---
+++ src/app/controllers/ReactionController.ts
@@ -0,0 +1,39 @@
+import Controller from '../../ts/abstract/Controller';
+
+import { IListener } from '../../ts/interface/IListener';
+
+import { SocketEndPoint } from '../../ts/enum/SocketEndPoint';
+import { DiscordAPIError } from 'discord.js';
+
+const { MessageAttachment, MessageEmbed } = require("discord.js");
+
+const GoogleImages = require("image-search-google");
+const googleImages = new GoogleImages(process.env.GOOGLE_CSE, process.env.GOOGLE_KEY);
+  
+class ReactionController extends Controller {
+  reaction: IListener = {
+    socket: SocketEndPoint.MESSAGE,
+
+    function: async message => {
+      if (!message || !this.isCallingBotCommand(message, 'react')) {
+        return;
+      }
+      
+      try {
+        const results = await googleImages.search("baka", { pages: 1 });
+        const reply = !results.length ? 
+          "No results" : 
+          new MessageEmbed()
+            .setTitle("Baka!")
+            .setDescription("+4 is a baka card! (*￣з￣)")
+            .setImage(results[Math.floor(Math.random() * results.length)].url);
+        message.channel.send(reply);
+      } catch(e) {
+        console.error(e);
+        message.channel.send("Something went wrong trying to send a reaction");
+      }
+    }
+  }
+}
+
+export default new ReactionController();
--- src/listeners.ts
+++ src/listeners.ts
@@ -4,13 +4,15 @@ import GreetingsController from './app/controllers/GreetingsController';
 import GameController from './app/controllers/GameController';
 import LikesController from './app/controllers/LikesController';
 import CardController from './app/controllers/CardController';
+import ReactionController from './app/controllers/ReactionController';
 
 const listeners: IListener[] = [
   GreetingsController.helloToServer,
   GreetingsController.sayHello,
   GameController.startGame,
   LikesController.likes,
-  CardController.card
+  CardController.card,
+  ReactionController.reaction
 ];
 
 export default listeners;
